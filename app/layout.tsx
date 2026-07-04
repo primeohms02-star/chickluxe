cf6572aac4fa084e62f043ea28b2239261daae96
prime Huncho <YOUR_GITHUB_EMAIL@example.com>
2026-07-04 21:16:50 +0100

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./style/responsive.css";
 import { ThemeProvider } from "./context/ThemeContext";
-
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -102,6 +102,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <>
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-0PJQ7LFCVD"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-0PJQ7LFCVD');
+    `}
+  </Script>
+</>
         <ThemeProvider>{children}</ThemeProvider>
 
         <script
